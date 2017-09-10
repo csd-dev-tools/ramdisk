f6372a0311f9758b54c8f4b238b568425d3a0897
Roy Nielsen <roy.nielsen17@gmail.com>
2017-09-09 21:51:15 -0600

    changed name

--- tests/test_with_pylint.py
+++ tests/test_with_pylint.py
@@ -1,77 +1,161 @@
-#!/usr/bin/python
-'''
-Runs pylint on all python .py files in a directory tree
+#!/usr/bin/python -u
 
-@author: Roy Nielsen
-'''
 from __future__ import absolute_import
 
-import re
 import os
+import re
 import sys
 import unittest
+from subprocess import Popen, PIPE
+from optparse import  OptionParser
 
 sys.path.append("..")
 
-from lib.CheckApplicable import CheckApplicable
-from lib.environment import Environment
-from lib.run_commands import RunWith
 from lib.loggers import CyLogger
 from lib.loggers import LogPriority as lp
+#from lib.run_commands import RunWith
+from tests.PylintIface import PylintIface, processFile
 
-class test_with_pylint(unittest.TestCase):
-    """
-    """
-    @classmethod
-    def setUpClass(self):
-        """
-        """
-        pass
+from pylint import epylint
 
-    def setUp(self):
-        """
-        """
-        self.environ = Environment()
-        self.logger = CyLogger()
-        self.logger.initializeLogs()
-        self.rw = RunWith(self.logger)
-        macApplicable = {'type' : 'white', 'os' : {'Mac OS X': ['10.10', 'r', '10.13.10']}}
-        linuxApplicable = {'type' : 'white', 'family': 'linux'}
+dirPkgRoot = '..'
+logger = CyLogger()
+logger.initializeLogs()
+#rw = RunWith(logger)
 
-        chkApp = CheckApplicable(self.environ, self.logger)
-        if chkApp.isApplicable(macApplicable):
-            self.pylint = "/usr/local/bin/pylint"
-        elif chkApp.isApplicable(linuxApplicable):
-            self.pylint = "/usr/bin/pylint"
-        else:
-            self.pylint = "/usr/bin/pylint"
+def getRecursiveTree(targetRootDir="."):
+    filesList = []
+    for root, dirs, files in os.walk(dirPkgRoot):
+        for myfile in files:
+            if re.search(".+\.py$", myfile): 
+                filesList.append(os.path.abspath(os.path.join(root, myfile)))
+    return filesList
 
-        self.dirPkgRoot = '..'
+def getDirList(targetDir="."):
+    filesList = []
+    for myfile in os.listdir(targetDir):
+        if re.search(".+\.py$", myfile): 
+            filesList.append(os.path.abspath(os.path.join(targetDir, myfile)))
+    return filesList
 
-    def test_for_errors(self):
-        """
-        """
-        for root, dirs, files in os.walk(self.dirPkgRoot):
-            for myfile in files:
-                if re.search(".+\.py$", myfile): # and not re.match("^%s$"%__file__, myfile):
-                    command = [self.pylint, os.path.join(root, myfile)]
-                    self.rw.setCommand(command)
-                    output, error, retcode = self.rw.communicate()
-                    for line in output.split("\n"):
-                        if re.match("^E:.+", line):
-                            self.assertTrue(False, str(os.path.join(root, myfile)) + " has error: " + str(line))
-                    for line in error.split("\n"):
-                        if re.match("^\s*E:\s+.+", line):
-                            self.assertTrue(False, str(os.path.join(root, myfile)) + " has error: " + str(line))
-        self.assertTrue(True, "...")
+def genTestData(fileList=[]):
+    test_case_data = []
+    if not fileList:
+        print "Cannot generate data from nothing..."
+        sys.exit(1)
 
-    @classmethod
-    def tearDownClass(self):
-        """
-        """
-        pass
+    pIface = PylintIface(logger)
+    for myfile in fileList:
+        #print myfile
+        try:
+            if not re.search(".+\.py$", myfile):
+                continue
+            elif not re.match("__init__.py", myfile):
+                output = ""
+                error = ""
+                jsonData = ""
+                #print myfile
+                jsonData = processFile(myfile)
+                jsonData = pIface.processFile(myfile)
+                #print jsonData
+                for item in jsonData:
+                    if re.match("^error$", item['type']) or re.match("^fatal$", item['type']):
+                        #print "Found: " + str(item['type']) + " (" + str(item['line']) + ") : " + str(item['message'])
+                        test_case_data.append((myfile, item['line'], item['message']))
+        except AttributeError:
+            pass
+    #for data in test_case_data:
+    #    print data
+    return test_case_data
 
-    def tearDown(self):
-        """
-        """
-        pass
+def pylint_test_template(*args):
+    '''
+    decorator for monkeypatching
+    '''
+    def foo(self):
+        self.assert_pylint_error(*args)
+    return foo
+
+
+class test_with_pylint_errors(unittest.TestCase):
+    def assert_pylint_error(self, myfile, lineNum, text):
+        self.assertTrue(False, myfile + ": (" + str(lineNum) + ") " + text)
+
+if __name__=="__main__":
+    #####
+    # Get options
+    parser = OptionParser(usage="\n\n%prog [options]\n\n", version="0.8.6")
+    
+    parser.add_option("-r", "--recursive-tree", dest="treeRoot",
+                      default="",
+                      help="The root of a directory to recurse and check all '*.py' files")
+    parser.add_option("--dir", dest="dirToCheck",
+                      default="",
+                      help="Name of the directory to look at for '*.py' files (not recursive)")
+    parser.add_option("-f", "--file", dest="fileToCheck",
+                      default=".",
+                      help="Name of the directory to look at for '*.py' files (not recursive)")
+    parser.add_option("-d", "--debug", action="store_true", dest="debug",
+                      default=0, help="Print debug messages")
+    parser.add_option("-v", "--verbose", action="store_true",
+                      dest="verbose", default=0,
+                      help="Print status messages")
+    
+    (opts, args) = parser.parse_args()
+
+    if opts.verbose != 0:
+        level = CyLogger(level=lp.INFO)
+    elif opts.debug != 0:
+        level = CyLogger(level=lp.DEBUG)
+    else:
+        level=lp.WARNING
+
+    if not opts.treeRoot and not opts.dirToCheck and not opts.fileToCheck:
+        output, error = Popen([__file__, "-h"], stdout=PIPE, stderr=PIPE).communicate()
+        sys.exit(0)
+
+    else:
+        #####
+        # Run unittest per options
+        if opts.treeRoot:
+            test_case_data = genTestData(getRecursiveTree(opts.treeRoot))
+        elif opts.dirToCheck:
+            test_case_data = genTestData(getDirList(opts.dirToCheck))
+        elif opts.fileToCheck:
+            test_case_data = genTestData([opts.fileToCheck])
+    for specificError in test_case_data:
+        #print str(specificError)
+        myfile, lineNum, text = specificError
+        test_name = "test_with_pylint_{0}_{1}_{2}".format("_".join("_".join(myfile.split("/")).split(".")), lineNum, "_".join("_".join(text.split(" ")).split("'")))
+        #print test_name
+        error_case = pylint_test_template(*specificError)
+        setattr(test_with_pylint_errors, test_name, error_case)
+    #####
+    # Initialize the test suite
+    test_suite = unittest.TestSuite()
+    
+    for specificError in test_case_data:
+        #print str(specificError)
+        myfile, lineNum, text = specificError
+        test_name = "test_with_pylint_{0}_{1}_{2}".format("_".join("_".join(myfile.split("/")).split(".")), lineNum, "_".join("_".join(text.split(" ")).split("'")))
+        #print test_name
+        error_case = pylint_test_template(*specificError)
+        setattr(test_with_pylint_errors, test_name, error_case)
+
+    test_suite.addTest(unittest.makeSuite(test_with_pylint_errors))
+    #test_suite.addTest(unittest.makeSuite(ConfigTestCase))
+    runner = unittest.TextTestRunner()
+    testResults  = runner.run(test_suite)  # output goes to stderr
+    
+else:
+    #####
+    # Run unittest per current source tree
+    test_case_data = genTestData(getRecursiveTree(".."))
+
+    for specificError in test_case_data:
+        #print str(specificError)
+        myfile, lineNum, text = specificError
+        test_name = "test_with_pylint_{0}_{1}_{2}".format("_".join("_".join(myfile.split("/")).split(".")), lineNum, "_".join("_".join(text.split(" ")).split("'")))
+        print test_name
+        error_case = pylint_test_template(*specificError)
+        setattr(test_with_pylint_errors, test_name, error_case)
